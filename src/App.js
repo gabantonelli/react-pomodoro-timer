0d80906755055c3bd00e38f1ef49a3608d94a167
Gabriel Antonelli <gabrielantonelli@hotmail.com>
2019-04-11 22:12:44 +0200

    added timer functionality

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import './App.css';
 class App extends Component {
   state = {
     breakLength: 5,
-    sessionLength: 25
+    sessionLength: 25,
+    status: 'Session',
+    minutesLeft: 25,
+    secondsLeft: 0,
+    running: false,
   }
 
   changeLength(subject, type) {
@@ -26,6 +30,33 @@ class App extends Component {
     }
   }
 
+  toggleTimer() {
+    //get current values
+    if (!this.state.running) {
+      this.setState({ running: true });
+      let minutes = this.state.minutesLeft;
+      let seconds = this.state.secondsLeft;
+      if (seconds === 0) {
+        seconds = 60;
+        minutes--;
+      }
+      const updateTimer = () => {
+        seconds--;
+        if (seconds < 0) {
+          seconds = 59;
+          minutes--;
+        }
+        this.setState({ minutesLeft: minutes, secondsLeft: seconds })
+      };
+      this.setState({ timer: setInterval(updateTimer, 1000) });
+    } else {
+      clearInterval(this.state.timer);
+      this.setState({ running: false });
+
+
+    }
+  }
+
   render() {
     return (
       <div className="App">
@@ -38,6 +69,10 @@ class App extends Component {
         <div id="session-length">{this.state.sessionLength}</div>
         <img src="./img/Reduce Button.png" alt="Decrement Session Length" onClick={() => this.changeLength('session', 'dec')} />
         <img src="./img/Increase Button.png" alt="Increase Sessopm Length" onClick={() => this.changeLength('session', 'inc')} />
+        <div id="timer-label">{this.state.status}</div>
+        <div id="time-left">{this.state.minutesLeft}:{this.state.secondsLeft}</div>
+        <img id="start_stop" src="./img/Play Button.png" alt="Start or Stop the timer" onClick={this.toggleTimer.bind(this)} />
+        <img id="reset" src="./img/Reset Button.png" alt="Start or Stop the timer" />
       </div>
     );
   }
